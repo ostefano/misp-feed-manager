f96eb7b81fe5db0ce036ed73258db843e07152d1
Stefano Ortolani <ortolanis@vmware.com>
2022-12-20 12:03:21 +0000

    Add some tests

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,13 +1,50 @@
+import ddt
 import unittest
+from feed_manager import translator
 
 
+@ddt.ddt
 class TestModule(unittest.TestCase):
     """Class to test the module."""
 
-    def test_foo_bar(self):
-        """Test the 'test_foo_bar' method."""
-        elems = [1, 2, 3]
-        self.assertEqual(len(elems), 3)
+    @ddt.data(
+        ("test=value", None, "value"),
+        ("misp-galaxy:malpedia=\"test\"", None, "test"),
+        ("misp-galaxy:malpedia=\"test\"", "misp-galaxy:malpedia", "test"),
+        ("misp-galaxy:malpedia=\"test\"", "misp-galaxy:another", None),
+        ("misp-galaxy:malpedia2=\"test2\"", None, "test2"),
+        (translator.TagUtils.create_tag("misp-galaxy:malpedia=\"test\""), None, "test"),
+    )
+    def test_get_cluster_tag_value(self, args):
+        """Test 'get_cluster_tag_value'."""
+        tag, category, expected_output = args
+        output = translator.TagUtils.get_cluster_tag_value(tag, category)
+        self.assertEqual(output, expected_output)
+
+    @ddt.data(
+        ("malpedia2=\"test2\"", ("malpedia2", "test2")),
+        ("misp-galaxy:malpedia=\"test\"", ("misp-galaxy:malpedia", "test")),
+        ("misp-galaxy:malpedia2=\"test2\"", ("misp-galaxy:malpedia2", "test2")),
+        (translator.TagUtils.create_tag("misp-galaxy:malpedia=\"test\""),
+         ("misp-galaxy:malpedia", "test")),
+    )
+    def test_get_cluster_category_and_value(self, args):
+        """Test 'get_cluster_category_and_value'."""
+        tag, expected_output = args
+        output = translator.TagUtils.get_cluster_category_and_value(tag)
+        self.assertEqual(output, expected_output)
+
+    @ddt.data(
+        ("malpedia2=\"test2\"", (None, None)),
+        ("misp-galaxy:malpedia=\"test\"", ("malpedia", "test")),
+        ("misp-galaxy:malpedia2=\"test2\"", ("malpedia2", "test2")),
+        (translator.TagUtils.create_tag("misp-galaxy:malpedia=\"test\""), ("malpedia", "test")),
+    )
+    def test_get_cluster_galaxy_and_value(self, args):
+        """Test 'get_cluster_galaxy_and_value'."""
+        tag, expected_output = args
+        output = translator.TagUtils.get_cluster_galaxy_and_value(tag)
+        self.assertEqual(output, expected_output)
 
 
 if __name__ == "__main__":
